f8463105f969730de69711d4fa5d6f87220532f2
Clare Tubridy <G00354146@atu.ie>
2023-05-03 13:43:23 +0100

    Summary of variables to .txt file

--- analysis.py
+++ analysis.py
@@ -2,3 +2,13 @@
 # Saves a histogram of each variable to png files, and
 # Outputs a scatter plot of each pair of variables.
 # Author: Clare Tubridy
+
+import pandas as pd
+import matplotlib as plt
+
+# Loads the Iris dataset from a CSV file
+iris_data = pd.read_csv('iris.csv')
+
+# Summary of each variable to a single text file
+with open('iris_summary.txt', 'w') as f:
+    f.write(iris_data.describe().to_string())
