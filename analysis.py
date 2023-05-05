3062e48eb957ae0edb6037ddde24bb2350dbce49
Clare Tubridy <G00354146@atu.ie>
2023-05-05 19:24:43 +0100

    Subplots Added

--- analysis.py
+++ analysis.py
@@ -11,19 +11,31 @@ import seaborn as sns
 iris_data = pd.read_csv('iris.csv')
 
 # Summary of each variable to a single text file
-with open('iris_summary.txt', 'w') as f:            # Opens files without having to use the close() function
-    f.write(iris_data.describe().to_string())       # Converts summary data to a string and writes it to a .txt file
+with open('iris_summary.txt', 'w') as f:                # Opens files without having to use the close() function
+    f.write(iris_data.describe().to_string())           # Converts summary data to a string and writes it to a .txt file
 
 # Histogram pf each variable to PNG files
-for col in iris_data.columns[:-1]:                  # Iterates over each column in the dataframe except the last one
+for col in iris_data.columns[:-1]:                      # Iterates over each column in the dataframe except the last one
     plt.hist(iris_data[col], bins = 8, color = 'green', edgecolor = 'black')   # Creates a histogram, with 10 bins, for each column
-    plt.xlabel(col)                                 # X label
-    plt.ylabel('Frequency')                         # Y label
-    plt.savefig(f'{col}_histogram.png')             # Saves histogram as a PNG file
-    plt.clf()                                       # Clears figure for the next plot
+    plt.xlabel(col)                                     # X label
+    plt.ylabel('Frequency')                             # Y label
+    plt.savefig(f'{col}_histogram.png')                 # Saves histogram as a PNG file
+    plt.clf()                                           # Clears figure for the next plot
 
 # Scatter plot of each pair of variables
+fig, axes = plt.subplots(1,2, figsize = (10,4))         # Create a figure with a 1x2 grid of subplots
+
+# Plot the first scatterplot in the first subplot
 sns.scatterplot(x = 'SepalLength', y = 'SepalWidth', 
-                hue = 'Species', data = iris_data)
-plt.legend(bbox_to_anchor = (1,1), loc = 'upper right')
-plt.show()
+                hue = 'Species', data = iris_data, ax = axes[0])
+axes[0].legend(bbox_to_anchor = (1,1), loc = 'upper left')
+axes[0].set_title('Sepal Scatter Plot')
+
+# Plot the second scatterplot in the second subplot
+sns.scatterplot(x = 'PetalLength', y = 'PetalWidth', 
+                hue = 'Species', data = iris_data, ax = axes[1])
+axes[1].legend(bbox_to_anchor = (1,1), loc = 'upper left')
+axes[1].set_title('Petal Scatter Plot')
+
+plt.tight_layout()                                      # Adjust the spacing between subplots
+plt.show()                                              # Show the plot
